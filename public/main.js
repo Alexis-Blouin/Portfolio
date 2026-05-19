55d575eebd1238274723d4864c501aea502e9c54
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2026-05-19 17:39:06 -0400

    Addition of a carousel and of my skills

--- public/main.js
+++ public/main.js
@@ -64,15 +64,76 @@ document.addEventListener("DOMContentLoaded", () => {
       closeAllModals();
     }
   });
+});
 
-  // Accordion
-  const accordions = document.querySelectorAll(".accordion");
+class BulmaAccordion {
+  constructor(node) {
+    this.accordion = node;
+    this.button = this.accordion.querySelector(".toggle");
+    this.init();
+  }
+  init() {
+    if (this.button)
+      this.button.addEventListener("click", () => this.togglePanel());
+  }
+  togglePanel() {
+    this.accordion.classList.toggle("is-active");
+  }
+}
 
-  accordions.forEach((accordion) => {
-    const header = accordion.querySelector(".accordion-header");
-    const body = accordion.querySelector(".accordion-body");
-    header.addEventListener("click", () => {
-      accordion.classList.toggle("is-active");
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".accordion").forEach((n) => new BulmaAccordion(n));
+});
+
+class BulmaCarousel {
+  constructor(node) {
+    this.element = node;
+    this.container = this.element.querySelector(".slider-container");
+    this.items = this.element.querySelectorAll(".slider-item");
+    this.prev = this.element.querySelector(".slider-navigation-previous");
+    this.next = this.element.querySelector(".slider-navigation-next");
+    this.pagination = this.element.querySelectorAll(".slider-page");
+    this.isVertical = this.container.classList.contains("is-vertical");
+    this.currentIndex = 0;
+    this.init();
+  }
+  init() {
+    if (this.prev) this.prev.addEventListener("click", () => this.move(-1));
+    if (this.next) this.next.addEventListener("click", () => this.move(1));
+    this.pagination.forEach((page) => {
+      page.addEventListener("click", () => {
+        this.currentIndex = parseInt(page.dataset.index);
+        this.update();
+      });
     });
-  });
+    window.addEventListener("resize", () => this.update());
+  }
+  move(delta) {
+    this.currentIndex =
+      (this.currentIndex + delta + this.items.length) % this.items.length;
+    this.update();
+  }
+  update() {
+    this.items.forEach((e) => this.transition(e));
+    this.pagination.forEach((page, i) => {
+      page.classList.toggle("is-active", i === this.currentIndex);
+    });
+  }
+  transition(item) {
+    var dir = "X";
+    if (this.isVertical) {
+      dir = "Y";
+    }
+    item.style.transform = `translate${dir}(${this.currentIndex * -100}%)`;
+    if (this.isVertical) {
+      for (let i = 0; i < Math.min(this.items.length, this.currentIndex); i++) {
+        this.items[i].style.transform =
+          `translate${dir}(${(this.items.length - this.currentIndex) * 100}%)`;
+      }
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".slider").forEach((n) => new BulmaCarousel(n));
 });
